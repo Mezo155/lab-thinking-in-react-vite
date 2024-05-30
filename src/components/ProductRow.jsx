36d7bb4bbbbf9ad940ebb644816f8b54b8668df1
Mezo155 <tonicb155@hotmail.com>
2024-05-30 20:07:06 +0200

    1-parte

---
+++ src/components/ProductRow.jsx
@@ -0,0 +1,15 @@
+function ProductRow ({product}){
+    const rowStyle = {
+        color: product.inStock ? "black" : "red"
+    }
+  
+return(
+    <tr style={rowStyle}>
+        <td>{product.name}</td>
+        <td>{product.price}</td>
+
+    </tr>
+)  
+}
+
+export default ProductRow
---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,22 @@
+import ProductRow from "./ProductRow"
+
+
+function ProductTable ({products}){
+return(
+    <table>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Price</td>
+            </tr>
+        </thead>
+        <tbody>
+            {products.map((product) => {
+               <ProductRow key={product.id} product={product}></ProductRow> 
+            })}
+        </tbody>
+    </table>
+)
+}
+
+export default ProductTable
